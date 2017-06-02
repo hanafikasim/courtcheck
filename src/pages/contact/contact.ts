d8cd1a76bf3962e16dd088d9ff9d58f6cb154680
hanafikasim <mhanafikasim@gmail.com>
2017-06-02 17:10:00 +0800

    dynamic list of news entry contains image

--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -1,14 +1,26 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 @Component({
   selector: 'page-contact',
   templateUrl: 'contact.html'
 })
-export class ContactPage {
-
+export class ContactPage{
   constructor(public navCtrl: NavController) {
 
   }
 
+  news = [
+    {
+      img: "https://www.askideas.com/media/30/Happy-Labour-Day-Malaysia.jpg",
+      title: "Labour Day",
+      desc: "Sports Hall will close on 1st May 2017."
+    },
+    {
+      img: "http://www.utm.my/sports/files/2017/04/Banner-USG-2017.jpg",
+      title: "UTM Staff Game 2017",
+      desc: "Visit www.utm.my/sports for mor info."
+    }
+    
+  ];
 }
